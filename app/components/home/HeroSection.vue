55417f6e7b3de11863d812d94fe3879d0969b26b
Peter Hoffman <peter@huestudios.com>
2026-02-26 17:33:07 -0500

    Updated absolute values

--- app/components/home/HeroSection.vue
+++ app/components/home/HeroSection.vue
@@ -226,7 +226,7 @@ onMounted(() => {
 
             <!-- Floating badge -->
             <div
-              class="absolute bottom-0 left-23.75 sm:-bottom-6 sm:-left-20 rounded-2xl t-shadow-lg border t-border p-4 sm:p-6 transform -rotate-2 t-bg-elevated"
+              class="absolute -bottom-4 left-23.75 sm:-bottom-6 sm:-left-20 md: left-10 rounded-2xl t-shadow-lg border t-border p-4 sm:p-6 transform -rotate-2 t-bg-elevated"
             >
               <div class="flex items-center gap-3 sm:gap-4">
                 <div
